73ba94e7b73b5791b3eb184b8e393eb0f337b6f9
tomhooijenga <tomhooijenga@gmail.com>
2016-05-25 15:13:38 +0200

    asdf

---
+++ app/ScrummerMobile/ScrummerMobile/www/scripts/login.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    "use strict";
+
+
+    document.addEventListener('deviceready', function () {
+        console.log(this);
+
+        var url = 'http://scrummer.space/api/oauth2/authorize/';
+
+        var target = "_blank";
+
+        var options = "location=yes,hidden=yes";
+
+        inAppBrowserRef = cordova.InAppBrowser.open(url, target, options);
+
+        inAppBrowserRef.addEventListener('loadstart', function (e) {
+            console.log(e);
+        });
+
+        inAppBrowserRef.addEventListener('loadstop', function (e) {
+            console.log(e);
+        });
+
+        inAppBrowserRef.addEventListener('loaderror', loadErrorCallBack);
+    }.bind(this), false);
+})()
