9d1b0f1304785ad70ff887116989d4e2604ae975
erikstelt <erikstelt@gmail.com>
2016-06-01 16:20:03 +0200

    Trophies page updated
    Still in progress. Basic HTML is ready

---
+++ app/ScrummerMobile/ScrummerMobile/www/scripts/trophies.js
@@ -0,0 +1,10 @@
+﻿// Scripts for the trophies page of the application
+(function () {
+    "use strict";
+
+    // When the document is loaded we set up the flickity slider
+    document.addEventListener('DOMContentLoaded', function () {
+        // Initialize main slider
+        alert("works");
+    });
+});
