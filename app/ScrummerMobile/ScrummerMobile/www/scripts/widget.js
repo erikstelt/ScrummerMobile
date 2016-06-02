b71621372a52acf9f81f653f89be3965ca077fc9
tomhooijenga <tomhooijenga@gmail.com>
2016-06-02 13:48:01 +0200

    Added automatic retry in widget

--- app/ScrummerMobile/ScrummerMobile/www/scripts/widget.js
+++ app/ScrummerMobile/ScrummerMobile/www/scripts/widget.js
@@ -1,10 +1,17 @@
 (function () {
 
+    var timeouts = [5000, 30000, 300000], // 5, 30, 300 secs
+        tries = 0,
+        render = templates.render.bind(templates, 'widget');
+
     /**
      * @returns {Promise}
      */
     templates.data.widget = function () {
         return api.getProfile().then(function (data) {
+            // Reset the attempts to fetch data
+            tries = 0;
+
             var level = Math.floor(Math.pow(data.exp / 2, 1 / 3));
 
             return {
@@ -17,8 +24,17 @@
                 power5: data.power5,
                 level: level
             }
-        }).catch(notify.show);
+        }).catch(function () {
+            // Try again in a while
+            window.setTimeout(render, timeouts[tries]);
+
+            if (tries < timeouts.length - 1) {
+                tries++;
+            }
+
+            notification.show("There was a network error");
+        });
     };
 
-    templates.render('widget');
+    render();
 })();
