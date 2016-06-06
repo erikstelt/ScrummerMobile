0856a202f675f836b2d7f899ace29ae203358fb9
erikstelt <erikstelt@gmail.com>
2016-06-06 15:01:56 +0200

    Added progress value and changed styling
    - Progress Value
    - Widget styling
    - Trophies styling

---
+++ app/ScrummerMobile/ScrummerMobile/www/scripts/trophies.js
@@ -0,0 +1,39 @@
+﻿(function () {
+
+    var timeouts = [5000, 30000, 300000], // 5, 30, 300 secs
+        tries = 0,
+        render = templates.render.bind(templates, 'trophies');
+
+    /**
+     * @returns {Promise}
+     */
+    templates.data.trophies = function () {
+        return api.getProfile().then(function (data) {
+            // Reset the attempts to fetch data
+            tries = 0;
+
+            // Set the values which we'd like to return
+            return {
+                power1: data.power1,
+                power2: data.power2,
+                power3: data.power3,
+                power4: data.power4,
+                power5: data.power5,
+            }
+        }).catch(function () {
+            // Try again in a while
+            window.setTimeout(render, timeouts[tries]);
+
+            if (tries < timeouts.length - 1) {
+                tries++;
+            }
+
+            notification.show("There was a network error");
+
+            // End the chain here, otherwise the template will be rendered without data
+            return Promise.reject();
+        });
+    };
+
+    render();
+})();
