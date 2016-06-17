8d013c3bdd913be48fedfbfa9e888677a60fa399
tomhooijenga <tomhooijenga@gmail.com>
2016-06-17 10:21:35 +0200

    Added new card design

--- app/ScrummerMobile/ScrummerMobile/www/scripts/cards.js
+++ app/ScrummerMobile/ScrummerMobile/www/scripts/cards.js
@@ -2,28 +2,6 @@
     'use strict';
 
     document.addEventListener('DOMContentLoaded', function () {
-        var cards = document.querySelector('.cards'),
-            cardsNav = cards.querySelector('.slider-nav');
-
-        var slider = new Flickity(cards.querySelector('.slider'), {
-                initialIndex: 1,
-                setGallerySize: false,
-                pageDots: false,
-                prevNextButtons: false
-            });
-
-        // Update the button state after a swipe
-        slider.on('cellSelect', function (e) {
-            cardsNav.querySelector('[data-slide-index="' + slider.selectedIndex + '"]').checked = true;
-        });
-
-        // Bind the main menu buttons to
-        [].forEach.call(cardsNav.querySelectorAll('input[type="radio"]'), function (input) {
-            input.addEventListener('change', function () {
-                if (this.checked) {
-                    slider.select(this.dataset.slideIndex);
-                }
-            });
-        });
+        // todo: render cards from api
     });
 })();
