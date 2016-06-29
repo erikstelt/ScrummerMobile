dcc71d2412835396ccc5e33d78fce867f356a83c
tomhooijenga <tomhooijenga@gmail.com>
2016-06-29 15:26:15 +0200

    Fix

--- app/ScrummerMobile/ScrummerMobile/www/scripts/cards.js
+++ app/ScrummerMobile/ScrummerMobile/www/scripts/cards.js
@@ -3,7 +3,7 @@
 
     var powerIcons = ['ton-li-chart-7', 'ton-li-eye', 'ton-li-speech-buble-3', 'ton-li-gear-1', 'ton-li-pen'];
     // Get profile
-    return API.getProfile().then(function (profile) {
+    API.getProfile().then(function (profile) {
         // Get cards that need to be verified by you
         return API.getCards(profile.email).then(function (cards) {
             var template = document.getElementById("cards-template").innerHTML;
@@ -19,17 +19,20 @@
                 } else if (card.is_verified === 2) {
                     card.statusclass = 'denied';
                     card.statustext = 'denied';
-                } else { card.statusclass = ""}
+                } else {
+                    card.statusclass = ""
+                }
                 card.description = card.description || 'No description given.';
                 card.deadline.class = card.deadline.class.split(' ').join('');
 
                 return card;
-            }).sort(function(a, b) {
+            }).sort(function (a, b) {
                 if (a.deadline.timestamp > b.deadline.timestamp) {
                     return 1;
                 } else if (b.deadline.timestamp > a.deadline.timestamp) {
                     return -1;
-                };
+                }
+
                 return 0;
             });
 
@@ -38,18 +41,21 @@
             });
         });
     });
+
     document.addEventListener('DOMContentLoaded', function () {
-        delegate(window.document.body, '*', function () {
-            console.log('works');
-        });
+        delegate(document.querySelector('.cards'), '.deny, .accept', function () {
+            var cardId = this.dataset.cardId,
+                status = this.classList.contains('accept');
+
+            API.verifyCard(cardId, status).then(function (response) {
+                if (!response.result) {
+                    return Promise.reject('Card could not be changed');
+                }
+
+                // Alter card status
+            }).catch(function (error) {
+                Notification.show(error);
+            });
+        })
     });
-    //delegate(document.querySelector(".cards"), ".deny, .accept", function() {
-       // var cardId = this.dataset.cardId;
-        //var status = this.classList.contains("accept");
-        //API.verifyCard(cardId, status);
-      //  console.log(cardId, status);
-    //});
-
-    var test = document.querySelector(".cards");
-
-    })();
+})();
